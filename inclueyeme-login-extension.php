fd5357d5965fd1dd18fa20929296342f80d5a65f
Jesus Nuñez <47543878+Cro22@users.noreply.github.com>
2021-01-06 04:20:34 -0400

    Check Update Resume

--- inclueyeme-login-extension.php
+++ inclueyeme-login-extension.php
@@ -5,7 +5,7 @@ Plugin Name: Incluyeme Login Extension
 Plugin URI: https://github.com/Cro22
 Description: Extension de funciones (Registro) para el Plugin WPJob Board
 Author: Jesus Nuñez
-Version: 3.2.6
+Version: 3.2.7
 Author URI: https://github.com/Cro22
 Text Domain: incluyeme-login-extension
 Domain Path: /languages
@@ -17,6 +17,8 @@ require_once plugin_dir_path(__FILE__) . 'include/menus/incluyeme_login_menu.php
 require_once plugin_dir_path(__FILE__) . 'include/resources/newInsertions.php';
 add_action('admin_init', 'incluyeme_requirements_Login_Extension');
 add_action('plugins_loaded', 'incluyemeLogin_loaderCheck');
+add_action('plugins_loaded', 'incluyeme_loadResume');
+add_action('plugins_loaded', 'incluyeme_loadMyResume');
 function plugin_name_i18n_incluyeme_login()
 {
     load_plugin_textdomain('plugin-name', false, dirname(plugin_basename(__FILE__)) . '/languages/');
@@ -60,16 +62,15 @@ function incluyemeLogin_loaderCheck()
         } else {
             copy($template, $route . '/wpjobboard/resumes/register.php');
         }
-        
-        $template = plugin_dir_path(__FILE__) . '/include/templates/resumes/my-resume.php';
-        $route = get_template_directory();
-        if (!file_exists($route . '/wpjobboard/resumes/my-resume.php')) {
-            mkdir($route . '/wpjobboard');
-            mkdir($route . '/wpjobboard/resumes');
-            copy($template, $route . '/wpjobboard/resumes/my-resume.php');
-        } else {
-            copy($template, $route . '/wpjobboard/resumes/my-resume.php');
-        }
+    }
+    
+}
+
+function incluyeme_loadResume()
+{
+    $version = '3.2.6';
+    $check = strcmp(get_option('incluyeme_loadResume'), $version);
+    if ($check === 0) {
         $template = plugin_dir_path(__FILE__) . '/include/templates/resumes/resume.php';
         $route = get_template_directory();
         if (!file_exists($route . '/wpjobboard/resumes/resume.php')) {
@@ -79,14 +80,28 @@ function incluyemeLogin_loaderCheck()
         } else {
             copy($template, $route . '/wpjobboard/resumes/resume.php');
         }
-        
-        newInsertions();
-        update_option('incluyemeLoginVersion', $version);
-        
+        update_option('incluyeme_loadResume', $version);
+    }
+}
+
+function incluyeme_loadMyResume()
+{
+    $version = '3.2.6';
+    $check = strcmp(get_option('loadResumeMy'), $version);
+    if ($check === 0) {
+        $template = plugin_dir_path(__FILE__) . '/include/templates/resumes/my-resume.php';
+        $route = get_template_directory();
+        if (!file_exists($route . '/wpjobboard/resumes/my-resume.php')) {
+            mkdir($route . '/wpjobboard');
+            mkdir($route . '/wpjobboard/resumes');
+            copy($template, $route . '/wpjobboard/resumes/my-resume.php');
+        } else {
+            copy($template, $route . '/wpjobboard/resumes/my-resume.php');
+        }
+        update_option('loadResumeMy', $version);
     }
     
 }
-
 
 require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
