9b7b8ddfaaafab2a83fc452309b61be1be7bdb21
Jesus Nuñez <jesus.nunez2050@gmail.com>
2023-09-28 18:20:17 -0400

    feat: update version

--- include/lib/abstracts/WP_Incluyeme_Countries_Abs.php
+++ include/lib/abstracts/WP_Incluyeme_Countries_Abs.php
@@ -1032,11 +1032,9 @@ abstract class WP_Incluyeme_Countries_Abs {
         global $wpdb;
         
         $myrows = $wpdb->get_results( "SELECT * from " . self::$prefersJobs . " WHERE id = ".$preferJobs );
-        print_r(error_log($myrows, true));
 		foreach ( $myrows as $details ) {
 			update_user_meta( $userID, 'area_interes', $details->jobs_prefers );
 			update_user_meta( $userID, 'area', $details->jobs_prefers );
-			  print_r(error_log(get_user_meta($userID, 'area_interes'), true));
 		}
 		
 		self::$wp->update( self::$incluyemeUsersInformation, [
