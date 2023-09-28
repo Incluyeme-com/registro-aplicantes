e432f503a540ce509c27cfb5e851dcf20f6d60ae
Jesus Nuñez <jesus.nunez2050@gmail.com>
2023-09-28 12:22:40 -0400

    feat: update version

--- include/verifications/register.php
+++ include/verifications/register.php
@@ -1,80 +1,93 @@
 <?php
-include_once dirname(__DIR__, 1) . '/lib/WP_Incluyeme_Login_Countries.php';
-header('Content-type: application/json');
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+include_once dirname( __DIR__, 1 ) . '/lib/WP_Incluyeme_Login_Countries.php';
+include_once dirname( __DIR__, 1 ) . '/lib/updateData.php';
+header( 'Content-type: application/json' );
+if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
 	$verifications = new WP_Incluyeme_Login_Countries();
-	if (isset($_POST['userID']) && isset($_POST['removeIMG'])) {
-		$verifications::deleteIMG($_POST['userID'], 1);
+	if ( isset( $_POST['userID'] ) && isset( $_POST['removeIMG'] ) ) {
+		$verifications::deleteIMG( $_POST['userID'], 1 );
 	}
-	if (isset($_POST['googleID'])) {
-		$email = $_POST['email'];
+	if ( isset( $_POST['googleID'] ) ) {
+		$email    = $_POST['email'];
 		$password = $_POST['password'];
-		$name = $_POST['name'];
+		$name     = $_POST['name'];
 		$lastName = $_POST['lastName'];
-		$response = $verifications::searchUserExistSocial($email, $password, $name, $lastName, $_POST['google'], $_POST['facebook']);
-		return;
-	}
-	if (isset($_POST['userID']) && isset($_POST['removeCUD'])) {
-		$verifications::deleteIMG($_POST['userID'], 3);
-	}
-	if (isset($_POST['userID']) && isset($_POST['RemoveCV'])) {
-		$verifications::deleteIMG($_POST['userID'], 2);
-	}
-	if (isset($_POST['userID']) && !empty($_FILES)) {
+		$response = $verifications::searchUserExistSocial( $email, $password, $name, $lastName, $_POST['google'], $_POST['facebook'] );
 		
-		if (isset($_POST['userID']) && isset($_FILES['img_path']) && !$_FILES['img_path']['error']) {
-			$verifications::updateIMG($_POST['userID'], $_FILES['img_path']);
-		}
-		if (isset($_POST['userID']) && isset($_FILES['cud']) && !$_FILES['cud']['error']) {
-			$verifications::updateCUD($_POST['userID'], $_FILES['cud']);
-		}
-		if (isset($_POST['userID']) && isset($_FILES['cv']) && !$_FILES['cv']['error']) {
-			$verifications::updateCV($_POST['userID'], $_FILES['cv']);
-		}
 		return;
 	}
-	if ((isset($_POST['google']) || isset($_POST['facebook'])) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['name']) && isset($_POST['lastName'])) {
-		$email = $_POST['email'];
+	if ( isset( $_POST['userID'] ) && isset( $_POST['removeCUD'] ) ) {
+		$verifications::deleteIMG( $_POST['userID'], 3 );
+	}
+	if ( isset( $_POST['userID'] ) && isset( $_POST['RemoveCV'] ) ) {
+		$verifications::deleteIMG( $_POST['userID'], 2 );
+	}
+	if ( isset( $_POST['userID'] ) && ! empty( $_FILES ) ) {
+		
+		if ( isset( $_POST['userID'] ) && isset( $_FILES['img_path'] ) && ! $_FILES['img_path']['error'] ) {
+			$verifications::updateIMG( $_POST['userID'], $_FILES['img_path'] );
+		}
+		if ( isset( $_POST['userID'] ) && isset( $_FILES['cud'] ) && ! $_FILES['cud']['error'] ) {
+			$verifications::updateCUD( $_POST['userID'], $_FILES['cud'] );
+		}
+		if ( isset( $_POST['userID'] ) && isset( $_FILES['cv'] ) && ! $_FILES['cv']['error'] ) {
+			$verifications::updateCV( $_POST['userID'], $_FILES['cv'] );
+		}
+		
+		return;
+	}
+	if ( ( isset( $_POST['google'] ) || isset( $_POST['facebook'] ) ) && isset( $_POST['email'] ) && isset( $_POST['password'] ) && isset( $_POST['name'] ) && isset( $_POST['lastName'] ) ) {
+		$email    = $_POST['email'];
 		$password = $_POST['password'];
-		$name = $_POST['name'];
+		$name     = $_POST['name'];
 		$lastName = $_POST['lastName'];
-		$response = $verifications::searchUserExistSocial($email, $password, $name, $lastName, $_POST['google'], $_POST['facebook']);
-		echo $verifications->json_response(200, $response);
+		$response = $verifications::searchUserExistSocial( $email, $password, $name, $lastName, $_POST['google'], $_POST['facebook'] );
+		echo $verifications->json_response( 200, $response );
+		
 		return;
 	}
-	$_POST = json_decode(file_get_contents("php://input"), true);
-	if (isset($_POST['email']) && isset($_POST['password']) && !isset($_POST['name']) && !isset($_POST['lastName'])) {
-		$email = sanitize_email($_POST['email']);
+	$_POST = json_decode( file_get_contents( "php://input" ), true );
+	if ( isset( $_POST['email'] ) && isset( $_POST['password'] ) && ! isset( $_POST['name'] ) && ! isset( $_POST['lastName'] ) ) {
+		$email    = sanitize_email( $_POST['email'] );
 		$password = $_POST['password'];
-		echo $verifications->json_response(200, $verifications::searchUserExist($email, $password));
+		echo $verifications->json_response( 200, $verifications::searchUserExist( $email, $password ) );
+		
 		return;
 	}
 	
-	if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['name']) && isset($_POST['lastName']) && isset($_POST['haveDiscap'])) {
-		$email = sanitize_email($_POST['email']);
-		$password = sanitize_email($_POST['password']);
-		$name = sanitize_text_field($_POST['name']);
-		$lastName = sanitize_text_field($_POST['lastName']);
+	if ( isset( $_POST['email'] ) && isset( $_POST['password'] ) && isset( $_POST['name'] ) && isset( $_POST['lastName'] ) && isset( $_POST['haveDiscap'] ) ) {
+		$email      = sanitize_email( $_POST['email'] );
+		$password   = sanitize_email( $_POST['password'] );
+		$name       = sanitize_text_field( $_POST['name'] );
+		$lastName   = sanitize_text_field( $_POST['lastName'] );
 		$haveDiscap = $_POST['haveDiscap'];
-		$response = $verifications::registerUser($email, $password, $name, $lastName,false, $haveDiscap);
+		$response   = $verifications::registerUser( $email, $password, $name, $lastName, false, $haveDiscap );
 		
-		echo $verifications->json_response(200, $response);
+		echo $verifications->json_response( 200, $response );
+		
 		return;
 	}
-	if (isset($_POST['userID']) && isset($_POST['country_edu'])) {
+	if ( isset( $_POST['userID'] ) && isset( $_POST['cudOption'] ) ) {
+		$update = new UpdateData();
 		
-		echo $verifications->json_response(200, $verifications::updateUsersEducation($_POST['dateStudieB'],
-			$_POST['dateStudiesD'],
-			$_POST['dateStudiesH'],
-			$_POST['eduLevel'],
-			$_POST['studies'],
-			$_POST['titleEdu'],
-			$_POST['university_edu'],
-			$_POST['university_otro'], $_POST['country_edu'], $_POST['userID']));
+		if ( isset( $_POST['edu_levelMaxSec'] ) ) {
+			$update->setMetaData( 'edu_levelMaxSec', $_POST['edu_levelMaxSec'] );
+		}
+		if ( isset( $_POST['country_edu'] ) ) {
+			echo $verifications->json_response( 200, $verifications::updateUsersEducation( $_POST['dateStudieB'],
+				$_POST['dateStudiesD'],
+				$_POST['dateStudiesH'],
+				$_POST['eduLevel'],
+				$_POST['studies'],
+				$_POST['titleEdu'],
+				$_POST['university_edu'],
+				$_POST['university_otro'], $_POST['country_edu'], $_POST['userID'] ) );
+		}
+		echo $verifications->json_response( 200, $_POST['userID']);
 		return;
 	}
-	if (isset($_POST['userID']) && isset($_POST['areaEmployed'])) {
-		echo $verifications->json_response(200, $verifications::updateUsersWorks($_POST['actuWork'],
+	if ( isset( $_POST['userID'] ) && isset( $_POST['areaEmployed'] ) ) {
+		echo $verifications->json_response( 200, $verifications::updateUsersWorks( $_POST['actuWork'],
 			$_POST['areaEmployed'],
 			$_POST['dateStudiesDLaboral'],
 			$_POST['dateStudiesHLaboral'],
@@ -82,11 +95,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			$_POST['jobs'],
 			$_POST['jobsDescript'],
 			$_POST['levelExperience'],
-			$_POST['userID']));
+			$_POST['userID'] ) );
+		
 		return;
 	}
-	if (isset($_POST['genre'])) {
-		echo $verifications->json_response(200, $verifications::updateUsersInformation($_POST['city'],
+	if ( isset( $_POST['genre'] ) ) {
+		$update = new UpdateData();
+		echo $verifications->json_response( 200, $verifications::updateUsersInformation( $_POST['city'],
 			$_POST['dateBirthDay'],
 			$_POST['fPhone'],
 			$_POST['fiPhone'],
@@ -94,30 +109,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			$_POST['mPhone'],
 			$_POST['state'],
 			$_POST['street'],
-			$_POST['phone']));
+			$_POST['phone'],
+			$_POST['street'] ) );
+		if ( isset( $_POST['country_nac'] ) ) {
+			$update->setMetaData( 'country_nac', $_POST['country_nac'] );
+		}
+		if ( isset( $_POST['livingZone'] ) ) {
+			$update->setMetaData( 'livingZone', $_POST['livingZone'] );
+		}
+		
 		return;
 	}
-	if (isset($_POST['userID']) && isset($_POST['discaps']) && isset($_POST['moreDis'])) {
-		$verifications::updateDiscapacidades($_POST['userID'], $_POST['discaps'], $_POST['moreDis']);
-		if (isset($_POST['userID']) && isset($_POST['motriz'])) {
-			$verifications::updateMotriz($_POST['userID'], $_POST['mPie'],
+	if ( isset( $_POST['userID'] ) && isset( $_POST['discaps'] ) && isset( $_POST['moreDis'] ) ) {
+		$verifications::updateDiscapacidades( $_POST['userID'], $_POST['discaps'], $_POST['moreDis'] );
+		if ( isset( $_POST['userID'] ) && isset( $_POST['motriz'] ) ) {
+			$verifications::updateMotriz( $_POST['userID'], $_POST['mPie'],
 				$_POST['mSen'],
 				$_POST['mEsca'],
 				$_POST['mBrazo'],
 				$_POST['peso'],
 				$_POST['mRueda'],
 				$_POST['desplazarte'],
-				$_POST['mDigi']);
+				$_POST['mDigi'] );
 		}
-		if (isset($_POST['userID']) && isset($_POST['auditiva'])) {
-			$verifications::updateAuditiva($_POST['aAmbient'],
+		if ( isset( $_POST['userID'] ) && isset( $_POST['auditiva'] ) ) {
+			$verifications::updateAuditiva( $_POST['aAmbient'],
 				$_POST['aSennas'],
 				$_POST['aLabial'],
 				$_POST['aBajo'],
-				$_POST['aImplante'], $_POST['aOral'], $_POST['aFluida'], $_POST['userID']);
+				$_POST['aImplante'], $_POST['aOral'], $_POST['aFluida'], $_POST['userID'] );
 		}
-		if (isset($_POST['userID']) && isset($_POST['visual'])) {
-			$verifications::updateVisual($_POST['userID'],
+		if ( isset( $_POST['userID'] ) && isset( $_POST['visual'] ) ) {
+			$verifications::updateVisual( $_POST['userID'],
 				$_POST['vLejos'],
 				$_POST['vObservar'],
 				$_POST['vColores'],
@@ -126,44 +149,56 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			);
 			
 		}
-		if (isset($_POST['userID']) && isset($_POST['visceral'])) {
-			$verifications::updateVisceral($_POST['userID'],
+		if ( isset( $_POST['userID'] ) && isset( $_POST['visceral'] ) ) {
+			$verifications::updateVisceral( $_POST['userID'],
 				$_POST['vHumedos'],
 				$_POST['vTemp'],
 				$_POST['vPolvo'],
 				$_POST['vCompleta'],
-				$_POST['vAdap']);
+				$_POST['vAdap'] );
 			
 		}
-		if (isset($_POST['userID']) && isset($_POST['intelectual'])) {
-			$verifications::updateIntelectual($_POST['userID'],
+		if ( isset( $_POST['userID'] ) && isset( $_POST['intelectual'] ) ) {
+			$verifications::updateIntelectual( $_POST['userID'],
 				$_POST['inteEscri'],
 				$_POST['inteTransla'],
 				$_POST['inteTarea'],
 				$_POST['inteActividad'],
 				$_POST['inteMolesto'],
 				$_POST['inteTrabajar'],
-				$_POST['inteTrabajarSolo']);
+				$_POST['inteTrabajarSolo'] );
 		}
-		echo $verifications->json_response(200, 'COMPLETADO');
+		echo $verifications->json_response( 200, 'COMPLETADO' );
+		
 		return;
 	}
 	
-	if (isset($_POST['userID']) && isset($_POST['idioms'])) {
-		$verifications::updateIdioms($_POST['userID'], $_POST['idioms'], $_POST['oLevel'], $_POST['wLevel'], $_POST['sLevel'], $_POST['idiomsOther']);
-		echo $verifications->json_response(200, 'COMPLETADO');
+	if ( isset( $_POST['userID'] ) && isset( $_POST['idioms'] ) ) {
+		$verifications::updateIdioms( $_POST['userID'], $_POST['idioms'], $_POST['oLevel'], $_POST['wLevel'], $_POST['sLevel'], $_POST['idiomsOther'] );
+		echo $verifications->json_response( 200, 'COMPLETADO' );
+		
 		return;
 	}
-	if (isset($_POST['userID']) && isset($_POST['preferJobs'])) {
-		$verifications::updatePrefersJobs($_POST['userID'], $_POST['preferJobs']);
-		echo $verifications->json_response(200, 'COMPLETADO');
+	if ( isset( $_POST['userID'] ) && isset( $_POST['preferJobs'] ) ) {
+		$update = new UpdateData();
+		$verifications::updatePrefersJobs( $_POST['userID'], $_POST['preferJobs'] );
+		if ( isset( $_POST['workingSearch'] ) ) {
+			$update->setMetaData( 'workingSearch', $_POST['workingSearch'] );
+		}
+		if ( isset( $_POST['workingNow'] ) ) {
+			$update->setMetaData( 'workingNow', $_POST['workingNow'] );
+		}
+		echo $verifications->json_response( 200, 'COMPLETADO' );
+		
 		return;
 	}
-	if (isset($_POST['userID']) && isset($_POST['meetingIncluyeme'])) {
-		$verifications::meetingIncluyeme($_POST['userID'], $_POST['meetingIncluyeme']);
-		echo $verifications->json_response(200, 'COMPLETADO');
+	if ( isset( $_POST['userID'] ) && isset( $_POST['meetingIncluyeme'] ) ) {
+		$verifications::meetingIncluyeme( $_POST['userID'], $_POST['meetingIncluyeme'] );
+		echo $verifications->json_response( 200, 'COMPLETADO' );
+		
 		return;
 	}
-	echo $verifications->json_response(200, 'COMPLETADO');
+	echo $verifications->json_response( 200, 'COMPLETADO' );
 }
+
 return;
